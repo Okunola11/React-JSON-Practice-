59d8e232b31741447dcd28a6d83bf233dcabd226
Okunola Abdulwasiu <abdwasiuayoadeokunola@gmail.com>
2023-11-19 21:42:39 +0100

    Fetched JSON data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import Contents from "./Contents";
+import Form from "./Form";
+import Table from "./Table";
 
 function App() {
+  const API_URL = `https://jsonplaceholder.typicode.com/`;
+
+  const [items, setItems] = useState([]);
+  const [request, setRequest] = useState("users");
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      try {
+        const fetchResponse = await fetch(`${API_URL}${request}`);
+        if (!fetchResponse.ok) throw Error("Could not fetch required data");
+        const data = await fetchResponse.json();
+        setItems(data);
+      } catch (err) {
+        console.log(err.message);
+      }
+    };
+    fetchItems();
+  }, [request, API_URL]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Form
+        items={items}
+        setItems={setItems}
+        request={request}
+        setRequest={setRequest}
+      />
+      {/* <Contents items={items} /> */}
+      <Table items={items} />
     </div>
   );
 }
